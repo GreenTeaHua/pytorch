8a97b717eb01215fa5c195d71422ed3886220385
peter <peter_jiachen@163.com>
2017-10-05 09:44:56 +0800

    Fix linking errors for MSVC

--- DLConvertor.h
+++ DLConvertor.h
@@ -4,13 +4,15 @@
 #include "ATen/ATen.h"
 #include "ATen/dlpack.h"
 
+#include "THP.h"
+
 // this convertor will:
 // 1) take a Tensor object and wrap it in the DLPack tensor object
 // 2) take a dlpack tensor and convert it to the Tensor object
 
 namespace at {
 
-DLTensor* toDLPack(const Tensor& src, DLTensor* dlTensor);
-Tensor fromDLPack(const DLTensor* src);
+THP_CLASS DLTensor* toDLPack(const Tensor& src, DLTensor* dlTensor);
+THP_CLASS Tensor fromDLPack(const DLTensor* src);
 
 } //namespace at
